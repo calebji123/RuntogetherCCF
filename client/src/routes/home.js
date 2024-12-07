e3be859217b97dd1b5f9b8ece08d605b91d4c622
iamcolekim <76983801+iamcolekim@users.noreply.github.com>
2024-12-06 20:30:07 -0500

    Created test component above Welcome section

--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -1,32 +1,33 @@
-import React from "react";
+import React from 'react';
 
-import WelcomeCard from "../component/welcomeCard/welcomeCard";
-import LinkSection from "../component/linkSection/linkSection";
-import ElectionSection from "../component/electionSection/electionSection";
-import { getAllUsers } from "../actions/loadInfo";
-
+import WelcomeCard from '../component/welcomeCard/welcomeCard';
+import LinkSection from '../component/linkSection/linkSection';
+import CkimTestComponent from '../component/ckim-test-component/ckim-test-component';
+import ElectionSection from '../component/electionSection/electionSection';
+import { getAllUsers } from '../actions/loadInfo';
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          candidates: [],
-          user: {},
+            candidates: [],
+            user: {},
         };
-      }
-    
+    }
+
     componentDidMount() {
         getAllUsers(this);
     }
-  render() {
-    return (
-      <>
-        <WelcomeCard parent={this} user={this.state.user} />
-        <ElectionSection candidates={this.state.candidates} />
-        <LinkSection />
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <CkimTestComponent />
+                <WelcomeCard parent={this} user={this.state.user} />
+                <ElectionSection candidates={this.state.candidates} />
+                <LinkSection />
+            </>
+        );
+    }
 }
 
 export default Home;
